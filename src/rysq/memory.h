7764e7266afa042a6b1b65137cc01745614be2b5
Eduard Valeyev <eduard@valeyev.net>
2019-12-08 16:15:50 -0500

    can grab cache size using syscltbyname, if available

--- src/rysq/memory.h
+++ src/rysq/memory.h
@@ -1,14 +1,50 @@
 #include <unistd.h>
 #include <cassert>
 
+#if defined(_SC_LEVEL1_DCACHE_SIZE) && defined(_SC_LEVEL2_DCACHE_SIZE)
+# define RYSQ_HAS_SC_LEVELX_DCACHE_SIZE
+#else
+# if __has_include(<sys/types.h>) && __has_include(<sys/sysctl.h>)
+#  include <sys/types.h>
+#  include <sys/sysctl.h>
+#  define RYSQ_HAS_SYSCTL
+# else
+#  error "do not know how to query L{1,2} (data) cache size"
+# endif
+#endif
+
 namespace rysq {
 
   inline size_t l1_cache_size() {
+#ifdef RYSQ_HAS_SC_LEVELX_DCACHE_SIZE
     return sysconf(_SC_LEVEL1_DCACHE_SIZE);
+#endif
+#ifdef RYSQ_HAS_SYSCTL
+    {
+      int64_t cacheSizeFromSysctl = 0;
+      size_t sz = sizeof(cacheSizeFromSysctl);
+      const auto retcode = sysctlbyname("hw.l1dcachesize", &cacheSizeFromSysctl,
+                                        &sz, nullptr, 0);
+      assert(retcode == 0);
+      return cacheSizeFromSysctl;
+    }
+#endif
   }
 
   inline size_t l2_cache_size() {
-    return sysconf(_SC_LEVEL2_CACHE_SIZE);
+#ifdef RYSQ_HAS_SC_LEVELX_DCACHE_SIZE
+    return sysconf(_SC_LEVEL2_DCACHE_SIZE);
+#endif
+#ifdef RYSQ_HAS_SYSCTL
+    {
+      int64_t cacheSizeFromSysctl = 0;
+      size_t sz = sizeof(cacheSizeFromSysctl);
+      const auto retcode = sysctlbyname("hw.l2cachesize", &cacheSizeFromSysctl,
+                                        &sz, nullptr, 0);
+      assert(retcode == 0);
+      return cacheSizeFromSysctl;
+    }
+#endif
   }
 
   template<typename T>
